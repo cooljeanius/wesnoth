ac77c190aa0f327c18b65836c23ed8ae8896f2ec
Eric Gallager <egall@gwmail.gwu.edu>
2024-09-22 06:13:57 -0400

    run `prettier` on javascript

--- data/tools/unit_tree/menu.js
+++ data/tools/unit_tree/menu.js
@@ -1,118 +1,120 @@
 /*
  * Popup menu implementation for the Wesnoth units database
  */
-var wmlunits_menu_setup = (function() {
-    var menus = [];
-    var visibleMenu = false;
+var wmlunits_menu_setup = function () {
+  var menus = [];
+  var visibleMenu = false;
 
-    //
-    // Legacy browser support functions (IE 9 only, really)
-    //
+  //
+  // Legacy browser support functions (IE 9 only, really)
+  //
 
-    function removeClass(e, classId)
-    {
-        var prevClasses = e.className.split(/\s+/),
-            newClasses = [];
+  function removeClass(e, classId) {
+    var prevClasses = e.className.split(/\s+/),
+      newClasses = [];
 
-        for (var i = 0; i < prevClasses.length; ++i) {
-            if (prevClasses[i] != classId) {
-                newClasses.push(prevClasses[i]);
-            }
-        }
-
-        e.className = newClasses.join(" ");
+    for (var i = 0; i < prevClasses.length; ++i) {
+      if (prevClasses[i] != classId) {
+        newClasses.push(prevClasses[i]);
+      }
     }
 
-    function toggleClass(e, classId)
-    {
-        var prevClasses = e.className.split(/\s+/),
-            newClasses = [],
-            foundClass = false;
+    e.className = newClasses.join(" ");
+  }
 
-        for (var i = 0; i < prevClasses.length; ++i) {
-            if (prevClasses[i] == classId) {
-                foundClass = true;
-            } else {
-                newClasses.push(prevClasses[i]);
-            }
-        }
+  function toggleClass(e, classId) {
+    var prevClasses = e.className.split(/\s+/),
+      newClasses = [],
+      foundClass = false;
 
-        if (!foundClass) {
-            newClasses.push(classId);
-        }
-
-        e.className = newClasses.join(" ");
-
-        return !foundClass;
+    for (var i = 0; i < prevClasses.length; ++i) {
+      if (prevClasses[i] == classId) {
+        foundClass = true;
+      } else {
+        newClasses.push(prevClasses[i]);
+      }
     }
 
-    //
-    // The main stuff
-    //
-
-    function toggleMenu(event, menu)
-    {
-        var menu = event.target.nextElementSibling;
-
-        hideMenus(menu);
-        visibleMenu = toggleClass(event.target, "menu-visible");
+    if (!foundClass) {
+      newClasses.push(classId);
     }
 
-    function hideMenus(skipMenu)
-    {
-        if (!visibleMenu) {
-            return;
-        }
+    e.className = newClasses.join(" ");
 
-        for (var i = 0; i < menus.length; ++i) {
-            if (menus[i] !== skipMenu) {
-                removeClass(menus[i].previousElementSibling, "menu-visible");
-            }
-        }
+    return !foundClass;
+  }
+
+  //
+  // The main stuff
+  //
+
+  function toggleMenu(event, menu) {
+    var menu = event.target.nextElementSibling;
+
+    hideMenus(menu);
+    visibleMenu = toggleClass(event.target, "menu-visible");
+  }
+
+  function hideMenus(skipMenu) {
+    if (!visibleMenu) {
+      return;
     }
 
-    function isMenuBox(e)
-    {
-        return e.className.search(/\bpopupmenu\b/) != -1;
+    for (var i = 0; i < menus.length; ++i) {
+      if (menus[i] !== skipMenu) {
+        removeClass(menus[i].previousElementSibling, "menu-visible");
+      }
     }
+  }
 
-    function isNavBar(e)
-    {
-        return e.className.search(/\bnavbar\b/) != -1;
-    }
+  function isMenuBox(e) {
+    return e.className.search(/\bpopupmenu\b/) != -1;
+  }
 
-    var navItems = document.getElementsByClassName("popupcontainer");
+  function isNavBar(e) {
+    return e.className.search(/\bnavbar\b/) != -1;
+  }
 
-    // Set event handlers for individual menu triggers.
-    for (var i = 0; i < navItems.length; ++i) {
-        var navItem = navItems[i],
-            menu = navItem.getElementsByClassName("popupmenu")[0];
+  var navItems = document.getElementsByClassName("popupcontainer");
 
-        menus.push(menu);
+  // Set event handlers for individual menu triggers.
+  for (var i = 0; i < navItems.length; ++i) {
+    var navItem = navItems[i],
+      menu = navItem.getElementsByClassName("popupmenu")[0];
 
-        var id = menu.id,
-            a = navItem.getElementsByClassName("popuptrigger")[0];
+    menus.push(menu);
 
-        a.addEventListener("click", function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-            toggleMenu(event);
-        }, false);
-    }
+    var id = menu.id,
+      a = navItem.getElementsByClassName("popuptrigger")[0];
 
-    // Dismiss all visible menus on click outside them.
-    document.addEventListener("click", function(event) {
-        if (!visibleMenu) {
-            return;
-        }
+    a.addEventListener(
+      "click",
+      function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        toggleMenu(event);
+      },
+      false,
+    );
+  }
 
-        var parent = event.target;
-        while(parent && !isMenuBox(parent) && !isNavBar(parent)) {
-            parent = parent.parentElement;
-        }
+  // Dismiss all visible menus on click outside them.
+  document.addEventListener(
+    "click",
+    function (event) {
+      if (!visibleMenu) {
+        return;
+      }
 
-        if (!parent || !isMenuBox(parent)) {
-            hideMenus();
-        }
-    }, false);
-});
+      var parent = event.target;
+      while (parent && !isMenuBox(parent) && !isNavBar(parent)) {
+        parent = parent.parentElement;
+      }
+
+      if (!parent || !isMenuBox(parent)) {
+        hideMenus();
+      }
+    },
+    false,
+  );
+};
